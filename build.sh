99073d488cc6c3108b94306a2a561e4696ba3bed
Trent Schafer <trent.schafer@gmail.com>
2017-01-24 21:05:05 +1100

    Put binaries in tgz

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 #build.sh: Build the program for multiple platforms for releases.
 #Move into the dist folder.
 #A list of possible platforms can be seen: https://golang.org/doc/install/source#environment
-#Also, referenced: 
+#Also, referenced:
 #- https://dave.cheney.net/2015/08/22/cross-compilation-with-go-1-5
 #- https://github.com/golang/go/wiki/WindowsCrossCompiling
 
@@ -57,3 +57,13 @@ mkdir -p ${BASEDIR}/win_64
 go build
 mv odl.exe ${BASEDIR}/win_64/odl.exe
 echo "Done"
+
+
+#Next, make archives of these for each platform
+echo "Creating archives"
+cd ${BASEDIR}
+tar cvzf linux.tgz linux_32/ linux_64/ --remove-files
+tar cvzf windows.tgz win_32/ win_64/ --remove-files
+tar cvzf osx.tgz osx_32/ osx_64/ --remove-files
+
+echo "All finished. Now, go release!"
