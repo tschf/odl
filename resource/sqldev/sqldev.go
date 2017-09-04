1082c7d8ca8190bf267291b68c3611926955c367
Trent Schafer <trent.schafer@gmail.com>
2017-09-04 00:01:41 -0400

    Add SQLDev 17.2. Close #27

--- resource/sqldev/sqldev.go
+++ resource/sqldev/sqldev.go
@@ -17,6 +17,62 @@ func GetSqldevResources() []*resource.OracleResource {
 
 	sqldevResources := []*resource.OracleResource{}
 
+	//17.2
+	sqldevResources = append(sqldevResources, &resource.OracleResource{
+		Component:    "sqldev",
+		Version:      "17.2",
+		File:         []string{"https://edelivery.oracle.com/akam/otn/java/sqldeveloper/sqldeveloper-17.2.0.188.1159-1.noarch.rpm"},
+		License:      "http://www.oracle.com/technetwork/licenses/sqldev-license-152021.html",
+		OS:           "linux-rpm",
+		Arch:         arch.Na,
+		Lang:         "na",
+		AcceptCookie: acceptCookie,
+	})
+
+	sqldevResources = append(sqldevResources, &resource.OracleResource{
+		Component:    "sqldev",
+		Version:      "17.2",
+		File:         []string{"https://edelivery.oracle.com/akam/otn/java/sqldeveloper/sqldeveloper-17.2.0.188.1159-no-jre.zip"},
+		License:      "http://www.oracle.com/technetwork/licenses/sqldev-license-152021.html",
+		OS:           "other",
+		Arch:         arch.Na,
+		Lang:         "na",
+		AcceptCookie: acceptCookie,
+	})
+
+	sqldevResources = append(sqldevResources, &resource.OracleResource{
+		Component:    "sqldev",
+		Version:      "17.2",
+		File:         []string{"https://edelivery.oracle.com/akam/otn/java/sqldeveloper/sqldeveloper-17.2.0.188.1159-macosx.app.zip"},
+		License:      "http://www.oracle.com/technetwork/licenses/sqldev-license-152021.html",
+		OS:           "osx",
+		Arch:         arch.Na,
+		Lang:         "na",
+		AcceptCookie: acceptCookie,
+	})
+
+	sqldevResources = append(sqldevResources, &resource.OracleResource{
+		Component:    "sqldev",
+		Version:      "17.2",
+		File:         []string{"https://edelivery.oracle.com/akam/otn/java/sqldeveloper/sqldeveloper-17.2.0.188.1159-no-jre.zip"},
+		License:      "http://www.oracle.com/technetwork/licenses/sqldev-license-152021.html",
+		OS:           "windows",
+		Arch:         arch.Na,
+		Lang:         "na",
+		AcceptCookie: acceptCookie,
+	})
+
+	sqldevResources = append(sqldevResources, &resource.OracleResource{
+		Component:    "sqldev-jdk",
+		Version:      "17.2",
+		File:         []string{"https://edelivery.oracle.com/akam/otn/java/sqldeveloper/sqldeveloper-17.2.0.188.1159-x64.zip"},
+		License:      "http://www.oracle.com/technetwork/licenses/sqldev-license-152021.html",
+		OS:           "windows",
+		Arch:         arch.X64,
+		Lang:         "na",
+		AcceptCookie: acceptCookie,
+	})
+
 	//4.2
 
 	sqldevResources = append(sqldevResources, &resource.OracleResource{
